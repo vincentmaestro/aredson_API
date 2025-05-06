48e01db52c4d9cecc66d282b2a88a5750f0701a1
vincentmaestro <maestroobika@gmail.com>
2025-05-06 17:15:52 +0100

    updates

--- api/index.js
+++ api/index.js
@@ -12,9 +12,19 @@ connectToDb();
 
 app.get("/", (req, res) => res.send("\\{^_^}/ hi!"));
 
+const whitelist = ['http://localhost:5173', 'https://aredson.com'];
+function validateOrigin(origin, callback) {
+    if(whitelist.indexOf(origin) !== -1) {
+        callback(null, true);
+    }
+    else {
+        callback(new Error('Not allowed by CORS'));
+    }
+}
+
+app.use(cors({ origin: validateOrigin, allowedHeaders: ['Content-Type', 'x-auth-token'], exposedHeaders: ['x-auth-token'], credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: 'https://aredson.com', allowedHeaders: ['Content-Type', 'x-auth-token'], exposedHeaders: ['x-auth-token'], credentials: true }));
 app.use('/api/user', user);
 app.use('/api/auth', authentication);
 app.use((ex, req, res) => {
